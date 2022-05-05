09f091655ee538bfb2e6077453af3b9225111975
Teku-Guy <gusmuratalla@gmail.com>
2022-05-05 02:11:41 -0700

    added beginning structures

---
+++ client/src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../assets/login.scss";
+
+function Login() {
+    return ;//return login info here
+}
+export default Login;
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../assets/signup.scss";
+
+function SignUp() {
+    return ;//return signup info here
+}
+export default SignUp;
