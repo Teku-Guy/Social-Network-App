0d46beee6a076ef101d2c4d1ec1c448b48e4ca0d
Teku-Guy <gusmuratalla@gmail.com>
2022-08-06 23:29:48 -0700

    added auth.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 
+import { FETH_THOUGHTS_QUERY } from "../utils/queries";
+
 function Home() {
+  const {loading, data} = useQuery(FETH_THOUGHTS_QUERY);
+  if(data){
+    console.log(data);
+  }
+
   return (
   	<div>
       <h1>Home Page</h1>
