7679a1d453af2e98975705e9f8196ddc09eebe1f
Teku-Guy <gusmuratalla@gmail.com>
2022-09-23 23:10:58 -0700

    start to implement images upload

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,14 +2,14 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { Avatar, Box, Button, Container, Grid, Paper, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 import Auth from "../utils/auth";
 import { FETCH_PROFILE_QUERY, FETCH_USER_POSTS_QUERY } from '../utils/queries';
 import PostCard from "../components/PostCard";
 
 function Profile(){
-  const user = Auth.loggedIn();
+  const user = Auth.getProfile();
   const { username } = useParams();
   const { data:  getUser } = useQuery(FETCH_PROFILE_QUERY, {
     variables: {
@@ -22,6 +22,29 @@ function Profile(){
     },
   });
   const { getPostByUser: posts } = {...data};
+
+  const checkUser = () => {
+    console.log(user)
+    if(user.data.username === username) {
+      return (
+        <Button variant="outlined" color="info" size="small">
+          Edit
+        </Button>
+      );
+    } else if(user.data === '') {
+      return (
+        <Button variant="outlined" color="info" size="small" as={Link} to="/login" sx={{ textDecoration: "none"}}>
+          Follow
+        </Button>
+      )
+    } else {
+      return (
+        <Button variant="outlined" color="info" size="small">
+          Follow
+        </Button>
+      )
+    }
+  }
 
   let profileLayout;
   if(!getUser){
@@ -54,9 +77,7 @@ function Profile(){
                   <Grid item xs={12} md={7} mx={{ xs: "auto", sm: 6, md:1 }}>
                   <Box display="flex" justifyContent="space-between" alignItems="center" mb={1}>
                     <Typography variant="h3">{username}</Typography>
-                    <Button variant="outlined" color="info" size="small">
-                      Follow
-                    </Button>
+                    {checkUser()}
                   </Box>
                   <Grid container spacing={3} mb={3}>
                     <Grid item>
